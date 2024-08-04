1a8f871fa3a2800eec22430da9779c021ce8b011
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-08-03 23:21:09 -0700

    I completed building the landing page, the colors might not be accurate but I did my best, Timothy and I decided to use a different picture and I already communicated about it with Jonathan

--- relying-party/src/pages/LandingPage.jsx
+++ relying-party/src/pages/LandingPage.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-
-import './LandingPage.css'
-
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import ExampleAccordions from '../components/ExampleAccordions';
+import './LandingPage.css';
+import Events from '../components/Events';
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 
 export default function LandingPage() {
-  const [count, setCount] = useState(0)
-
+  const imagePath = '/volunteerimage.jpg'
   return (
-    <div id="landing-page">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='landing-page'>
+      <h2>Trending Events</h2>
+      <Events header={"Example Event 1"} paragraph={"This is an example of event description. All events will have a description field in the database which can be used here"}/>
+      <Events header={"Example Event 2"} paragraph={"This is an example of event description. All events will have a description field in the database which can be used here"}/>
+      <h2>Today's Inspiration</h2>
+      <div className='image-container'>
+        <img className='image' src={imagePath} alt="Image of a group of volunteers" />
+        <h3 className='text-over-image'>Example volunteer story title</h3>
+        <div className='read-more-container'>
+          <p className='read-more'>READ MORE</p>
+          <a href="" className='arrow-icon'><ArrowCircleRightOutlinedIcon/></a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <ExampleAccordions />
     </div>
-  )
+  );
 }
+
