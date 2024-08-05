1b835c0f3c534ba0b9953efd630e0c8e8f656108
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-08-05 16:32:43 -0700

    Added the route to the landing page inside of main

--- relying-party/src/main.jsx
+++ relying-party/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Router from 'react-router-dom'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -7,10 +8,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './main.css'
 
-import App from './pages/LandingPage.jsx'
+import LandingPage from './pages/LandingPage.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
