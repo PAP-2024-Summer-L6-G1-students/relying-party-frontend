4de9ae2f28ecb0278ba3552e2842e2923a34efcd
cindyn04 <cindyn037@gmail.com>
2024-08-13 13:33:35 -0700

    fixed sign in button

--- relying-party/src/pages/FindEventsPage.jsx
+++ relying-party/src/pages/FindEventsPage.jsx
@@ -33,12 +33,9 @@ const FindEventsPage = () => {
             <h1> Find Volunteer Opportunities</h1>
             <p className='search-description'>The search bar is a powerful tool that allows users to quickly and efficiently discover volunteer opportunities tailored to their preferences. As users type keywords into the search bar, the website dynamically filters and displays events that match the input, searching through event names and volunteer types. This real-time search functionality ensures that users can immediately see relevant results without having to navigate through numerous listings, making it easier to find the perfect volunteer event.</p>
             <p className='reminder'> Must have an account with (name of website) to participate. </p>
-            <button onClick={handleSignInClick} className='sign-in-button'>
-                Sign In
-            </button>
             <h2> Search for Volunteer Opportunities </h2>
             <SearchBar events={events} />
-            <CheckboxLabels/>
+            {/* <CheckboxLabels/> */}
             <Footer/>
         </div>
         
